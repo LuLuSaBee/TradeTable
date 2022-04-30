7495a66d2ca45c8eb8ef327e0827787e11abcc11
LuLuSaBee <lulusabeetw@gmail.com>
2022-04-30 15:50:13 +0800

    Initial Commit

---
+++ TradeTable/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TradeTable
+//
+//  Created by 劉聖龍 on 2022/4/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
